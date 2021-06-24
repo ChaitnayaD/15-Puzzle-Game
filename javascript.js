0ed813244c592e65cef3b7b683cec21348e42e3a
ChaitanyaD <crdhuware07@gmail.com>
2021-06-24 10:17:27 +0530

    commit3

--- javascript.js
+++ javascript.js
@@ -9,8 +9,8 @@ function shuffle() {
 for (var row=1;row<=4;row++) { //For each row of the 3x3 grid
    for (var column=1;column<=4;column++) { //For each column in this row
   
-    var row2=Math.floor(Math.random()*4 + 1); //Pick a random row from 1 to 3
-    var column2=Math.floor(Math.random()*4 + 1); //Pick a random column from 1 to 3
+    var row2=Math.floor(Math.random()*4 + 1); //Pick a random row from 1 to 4
+    var column2=Math.floor(Math.random()*4 + 1); //Pick a random column from 1 to 4
      
     swapTiles("cell"+row+column,"cell"+row2+column2); //Swap the look & feel of both cells
   } 
@@ -109,6 +109,8 @@ else{
 
 //Display updated time values to user
 document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+localStorage.setItem("seconds",seconds);
+
 }
 window.setInterval(stopwatch,1000);
 
@@ -138,6 +140,21 @@ function reset(){
     
     status = "0";
     start();
+    
+  }
+} 
+function reset1(){
+  if(status1=="reset" ){
+    count=0;
+    document.getElementById("displaymoves").innerHTML= "Moves:"+count;
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById("display").innerHTML = "00:00:00";
+    
+    status = "0";
+   
     
   }
 } 
