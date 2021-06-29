4d1c8e8dc64b09286b85c492edc4e773e74680df
ChaitanyaD <crdhuware07@gmail.com>
2021-06-29 17:23:11 +0530

    backfix

--- javascript.js
+++ javascript.js
@@ -220,3 +220,23 @@ function bg2(){
     function bg4(){
       document.getElementById("content").style.backgroundImage= "url('images/header.jpg')";
       }
+
+
+
+      function myFunction() {
+        document.getElementById("mydrop").classList.toggle("show");
+      }
+      
+      // Close the dropdown if the user clicks outside of it
+      window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
